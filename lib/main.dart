9065d699e8b1fab9b60f1689476c1559de7c05f8
Ale Bark Bruneri <ale.bark_bruneri@hotmail.com>
2019-06-25 00:06:17 -0300

    form validation

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,14 @@ class _HomeState extends State<Home> {
   TextEditingController heightController = TextEditingController();
   String _infoText = 'Please enter your details above';
 
+  GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+
   void _resetFields() {
     weightController.text = "";
     heightController.text = "";
     setState(() {
       _infoText = 'Please enter your details above';
+      _formKey = GlobalKey<FormState>();
     });
   }
 
@@ -56,48 +59,66 @@ class _HomeState extends State<Home> {
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(15, 15, 15, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.person_outline, size: 120, color: Colors.grey),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Weight (kg)",
-                  labelStyle: TextStyle(color: Colors.grey)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.grey, fontSize: 20.0),
-              controller: weightController,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Height (cm)",
-                  labelStyle: TextStyle(color: Colors.grey)),
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.grey, fontSize: 20.0),
-              controller: heightController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
-              child: Container(
-                height: 50.0,
-                child: RaisedButton(
-                  onPressed: _calculate,
-                  child: Text(
-                    'Calculate',
-                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120, color: Colors.grey),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Weight (kg)",
+                    labelStyle: TextStyle(color: Colors.grey)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 20.0),
+                controller: weightController,
+                validator:(value){
+                  if(value.isEmpty){
+                    return "Incorrect weight";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Height (cm)",
+                    labelStyle: TextStyle(color: Colors.grey)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 20.0),
+                controller: heightController,
+                validator: (value){
+                  if(value.isEmpty){
+                    return "Incorrect height";
+                  }
+                },
+
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        _calculate();
+                      }
+                    },
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    color: Colors.blueGrey,
                   ),
-                  color: Colors.blueGrey,
                 ),
               ),
-            ),
-            Text(
-              _infoText,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.grey, fontSize: 20.0),
-            )
-          ],
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 20.0),
+              )
+            ],
+          ),
         ),
       ),
     );
